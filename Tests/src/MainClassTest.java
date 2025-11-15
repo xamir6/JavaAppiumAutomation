ed835d744215be5205d6c326a6cb0dcbce5f431e
xamir6 <your-email@example.com>
2025-11-15 17:11:16 +0300

    testGetLocalNumber()

--- Tests/src/MainClassTest.java
+++ Tests/src/MainClassTest.java
@@ -3,24 +3,11 @@ import org.junit.Test;
 
 public class MainClassTest {
 
-    private int class_number = 20;
+    MainClass mainClass = new MainClass();
 
     @Test
     public void testGetLocalNumber() {
-        Assert.assertEquals("getLocalNumber() is not equals 14", 14, getLocalNumber());
-    }
-
-    @Test
-    public void testGetClassNumber() {
-        Assert.assertTrue("getClassNumber() is less than 45", getClassNumber() > 45);
-    }
-
-    public int getLocalNumber() {
-        return 14;
-    }
-
-    public int getClassNumber() {
-        return class_number;
+        Assert.assertEquals("getLocalNumber() is not equals 14", 14, mainClass.getLocalNumber());
     }
 
 }
